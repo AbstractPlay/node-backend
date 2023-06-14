8b78abd3e9335ec8a4e78ec323e13375908fff06
Aaron Dalton <aaron@daltons.ca>
2023-06-14 08:49:21 -0600

    Further tweaks

--- utils/yourturn.ts
+++ utils/yourturn.ts
@@ -57,10 +57,10 @@ export const handler: Handler = async (/*event: EventBridgeEvent<any,any>, conte
                 ReturnConsumedCapacity: "INDEXES",
             })
         );
-        console.log(`Consumed capacity: ${data?.ConsumedCapacity}`);
+        console.log(`Consumed capacity: ${JSON.stringify(data?.ConsumedCapacity)}`);
         let games = data?.Items as PartialGame[];
         if (games !== undefined) {
-            games = games.filter(g => ("toMove" in g) && (g.toMove !== undefined) && (g.toMove !== null));
+            games = games.filter(g => ("toMove" in g) && (g.toMove !== undefined) && (g.toMove !== null) && (g.toMove.toString().length > 0) );
         }
         console.log(JSON.stringify(games, null, 2));
 
