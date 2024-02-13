d9e51205f3a9531a95e930ba287ed00098e7a44a
pvanwamelen <paul.vanwamelen@cambiumassessment.com>
2024-02-12 22:59:51 -0500

    Again

--- api/abstractplay.ts
+++ api/abstractplay.ts
@@ -4864,7 +4864,7 @@ async function reportProblem(pars: { error: string })
   const users = data.Items;
   const playerIDs = [];
   for (const user of users!)
-    if (user.name === 'fritzd' || user.name === 'Perlkönig')
+    if (user.name === 'fritzd' || user.name === 'Fritz Deelman' || user.name === 'Perlkönig')
       playerIDs.push(user.sk);
   console.log("playerIDs", playerIDs);
   const errorAdmins = await getPlayers(playerIDs);
