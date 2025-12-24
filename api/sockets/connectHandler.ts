04406f6c605fdaf04655d5c0a7f97440c02afe0f
Aaron Dalton <aaron@daltons.ca>
2025-12-24 14:08:16 -0700

    Minimal connect handler

--- api/sockets/connectHandler.ts
+++ api/sockets/connectHandler.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+// import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+// import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 
 interface WebSocketConnectEvent {
   requestContext: {
@@ -14,39 +14,45 @@ interface WebSocketConnectEvent {
   };
 }
 
-const ddb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+// const ddb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
-export const handler = async (event: WebSocketConnectEvent) => {
-  console.log("Connect event:", JSON.stringify(event));
-  try {
-    const { connectionId, authorizer } = event.requestContext;
 
-    if (!authorizer?.userId) {
-        console.error("Missing userId in authorizer context");
-        return { statusCode: 401 };
-    }
-
-    const userId = authorizer.userId;
-
-    await ddb.send(
-        new PutCommand({
-        TableName: process.env.ABSTRACT_PLAY_TABLE!,
-        Item: {
-            pk: "wsConnections",
-            sk: connectionId,
-
-            connectionId,
-            userId,
-
-            // Optional TTL for auto-cleanup
-            ttl: Math.floor(Date.now() / 1000) + 3600,
-        },
-        })
-    );
-  } catch (ex) {
-    console.log("Connect error:", JSON.stringify(ex));
-    return { statusCode: 500 };
-  }
-
+exports.handler = async (event: WebSocketConnectEvent) => {
+  console.log("Connect invoked", JSON.stringify(event));
   return { statusCode: 200 };
 };
+
+// export const handler = async (event: WebSocketConnectEvent) => {
+//   console.log("Connect event:", JSON.stringify(event));
+//   try {
+//     const { connectionId, authorizer } = event.requestContext;
+
+//     if (!authorizer?.userId) {
+//         console.error("Missing userId in authorizer context");
+//         return { statusCode: 401 };
+//     }
+
+//     const userId = authorizer.userId;
+
+//     await ddb.send(
+//         new PutCommand({
+//         TableName: process.env.ABSTRACT_PLAY_TABLE!,
+//         Item: {
+//             pk: "wsConnections",
+//             sk: connectionId,
+
+//             connectionId,
+//             userId,
+
+//             // Optional TTL for auto-cleanup
+//             ttl: Math.floor(Date.now() / 1000) + 3600,
+//         },
+//         })
+//     );
+//   } catch (ex) {
+//     console.log("Connect error:", JSON.stringify(ex));
+//     return { statusCode: 500 };
+//   }
+
+//   return { statusCode: 200 };
+// };
