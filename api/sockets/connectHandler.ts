d0a025c43ea532d56f6a3ab3dcffa9a7e32fc65f
Aaron Dalton <aaron@daltons.ca>
2025-12-24 14:00:48 -0700

    Tweaking timeouts and adding try/catch

--- api/sockets/connectHandler.ts
+++ api/sockets/connectHandler.ts
@@ -18,30 +18,35 @@ const ddb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 export const handler = async (event: WebSocketConnectEvent) => {
   console.log("Connect event:", JSON.stringify(event));
-  const { connectionId, authorizer } = event.requestContext;
+  try {
+    const { connectionId, authorizer } = event.requestContext;
 
-  if (!authorizer?.userId) {
-    console.error("Missing userId in authorizer context");
-    return { statusCode: 401 };
+    if (!authorizer?.userId) {
+        console.error("Missing userId in authorizer context");
+        return { statusCode: 401 };
+    }
+
+    const userId = authorizer.userId;
+
+    await ddb.send(
+        new PutCommand({
+        TableName: process.env.ABSTRACT_PLAY_TABLE!,
+        Item: {
+            pk: "wsConnections",
+            sk: connectionId,
+
+            connectionId,
+            userId,
+
+            // Optional TTL for auto-cleanup
+            ttl: Math.floor(Date.now() / 1000) + 3600,
+        },
+        })
+    );
+  } catch (ex) {
+    console.log("Connect error:", JSON.stringify(ex));
+    return { statusCode: 500 };
   }
-
-  const userId = authorizer.userId;
-
-  await ddb.send(
-    new PutCommand({
-      TableName: process.env.ABSTRACT_PLAY_TABLE!,
-      Item: {
-        pk: "wsConnections",
-        sk: connectionId,
-
-        connectionId,
-        userId,
-
-        // Optional TTL for auto-cleanup
-        ttl: Math.floor(Date.now() / 1000) + 3600,
-      },
-    })
-  );
 
   return { statusCode: 200 };
 };
