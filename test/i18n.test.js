d03106cfa6a02f53ce41ed83fc0ae150d8e24277
Aaron Dalton <aaron@daltons.ca>
2026-09-02 11:12:02 -0600

    Translatable game names

--- test/i18n.test.js
+++ test/i18n.test.js
@@ -6,12 +6,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const node_test_1 = require("node:test");
 const strict_1 = __importDefault(require("node:assert/strict"));
 const i18next_1 = __importDefault(require("i18next"));
+const gameslib_1 = require("@abstractplay/gameslib");
+const gameDisplayName_1 = require("../lib/gameDisplayName");
 const abstractplay_1 = require("../api/abstractplay");
 (0, node_test_1.beforeEach)(async () => {
     if (i18next_1.default.isInitialized) {
         await i18next_1.default.changeLanguage('en');
     }
     await (0, abstractplay_1.initi18n)('en');
+});
+(0, node_test_1.test)('initi18n loads apgames bundles for managed languages', async () => {
+    strict_1.default.ok(i18next_1.default.hasResourceBundle('de', 'apgames'));
+    strict_1.default.ok(i18next_1.default.hasResourceBundle('en', 'apgames'));
+});
+(0, node_test_1.test)('localizedGameName resolves via active language after changeLanguageForPlayer', async () => {
+    const uid = 'hex';
+    const fallback = gameslib_1.gameinfo.get(uid)?.name ?? uid;
+    await (0, abstractplay_1.changeLanguageForPlayer)({ language: 'en' });
+    strict_1.default.equal((0, gameDisplayName_1.localizedGameName)(uid), fallback);
+    await (0, abstractplay_1.changeLanguageForPlayer)({ language: 'de' });
+    strict_1.default.equal(typeof (0, gameDisplayName_1.localizedGameName)(uid), 'string');
+    strict_1.default.ok((0, gameDisplayName_1.localizedGameName)(uid).length > 0);
 });
 (0, node_test_1.test)('changeLanguageForPlayer uses German when registered', async () => {
     await (0, abstractplay_1.changeLanguageForPlayer)({ language: 'de' });
